5fb12585d03a95991d57f4fd5a9c18492017e6bf
Hololens Dev Pc <bitter.daniel@gmail.com>
2019-09-17 07:14:36 -0500

    Refactored update to 3 method calls

--- Assets/AzureKinectDK/Examples/Scripts/DebugRenderer.cs
+++ Assets/AzureKinectDK/Examples/Scripts/DebugRenderer.cs
@@ -63,88 +63,104 @@ public class DebugRenderer : PersistantSingleton<DebugRenderer>
     {
         if (canUpdate)
         {
-            //this streams camera output as a texture to a plane in the scene
-            using (Capture capture = device.GetCapture())
-            {
-                tracker.EnqueueCapture(capture);
-                var color = capture.Color;
-                if (color.WidthPixels > 0)
-                {
-                    Texture2D tex = new Texture2D(color.WidthPixels, color.HeightPixels, TextureFormat.BGRA32, false);
-                    tex.LoadRawTextureData(color.GetBufferCopy());
-                    tex.Apply();
-                    renderer.material.mainTexture = tex;
-                }
-            }
-
-            //this gets skeleton data from frames and pulls individual joint data from the skeleton to apply to blocks that represent joints
-            using (var frame = tracker.PopResult())
-            {
-                Debug.LogFormat("{0} bodies found.", frame.NumBodies);
-                if (frame.NumBodies > 0)
-                {
-                    var bodyId = frame.GetBodyId(0);
-                    //Debug.LogFormat("bodyId={0}", bodyId);
-                    this.skeleton = frame.GetSkeleton(0);
-                    skeletons.Add(this.skeleton);
-					for (var i = 0; i < (int)JointId.Count; i++)
-					{
-						var joint = this.skeleton.Joints[i];
-						var pos = joint.Position;
-						Debug.Log("pos: " + (JointId)i + " " + pos[0] + " " + pos[1] + " " + pos[2]);
-						var rot = joint.Orientation;
-						Debug.Log("rot " + (JointId)i + " " + rot[0] + " " + rot[1] + " " + rot[2] + " " + rot[3]); // Length 4
-						var v = new Vector3(pos[0], -pos[1], pos[2]) * 0.004f;
-						var r = new Quaternion(rot[1], rot[2], rot[3], rot[0]);
-						var obj = debugObjects[i];
-						obj.transform.SetPositionAndRotation(v, r);
-					}
-                }
-            }
+			streamCameraAsTexture();
+			captureSkeletons();
 			if (skeletons.Count > 4) // and the current scene is CaptureScene
 			{
-				Debug.Log("creating blockman 2 stuff");
-				blockman2 = GameManager.Instance.blockmanCaptured;
-				foreach (GameObject go in blockman2)
-				{
-					go.SetActive(true);
-				}
-
-				// skeletons is a List<Skeleton> of size 5
-				for (int i = 0; i < (int)JointId.Count; i++)
-				{
-					List<Vector3> positionsOfSameJointPositions = new List<Vector3>();
-					
-					for (int j = 0; j < skeletons.Count; j++)
-					{
-						float[] pos = skeletons[j].Joints[i].Position;
-						
-						Vector3 posV3 = new Vector3(pos[0], -pos[1], pos[2]) * 0.004f;
-						positionsOfSameJointPositions.Add(posV3);
-						
-					}
-					Vector3 averageOfSingleJointI = Vector3Helper.FindAveragePosition(positionsOfSameJointPositions);
-
-					float[] rot = skeletons[0].Joints[i].Orientation;
-					Quaternion rotationOfFirstSkeleton = new Quaternion(rot[1], rot[2], rot[3], rot[0]);
-
-					var jointCube = blockman2[i];
-					jointCube.transform.SetPositionAndRotation(averageOfSingleJointI, rotationOfFirstSkeleton);
-
-				}
-
-				Debug.Log("we have enough skeletons");
-				//GameManager.Instance.currentState = GameState.CaptureCompleted;
-				//Disable this Update loop's logic from running
-				canUpdate = false;
-				//GameManager.Instance.LoadScene((int)SceneEnums.Scenes.GetReady);
+				averageOutSkeletons();
 			}
 
 		}//end if(canUpdate) 
 	}//end Update()
 
+	void streamCameraAsTexture()
+	{
+		//this streams camera output as a texture to a plane in the scene
+		using (Capture capture = device.GetCapture())
+		{
+			tracker.EnqueueCapture(capture);
+			var color = capture.Color;
+			if (color.WidthPixels > 0)
+			{
+				Texture2D tex = new Texture2D(color.WidthPixels, color.HeightPixels, TextureFormat.BGRA32, false);
+				tex.LoadRawTextureData(color.GetBufferCopy());
+				tex.Apply();
+				renderer.material.mainTexture = tex;
+			}
+		}
+	}
+
+	void captureSkeletons()
+	{
+		//this gets skeleton data from frames and pulls individual joint data from the skeleton to apply to blocks that represent joints
+		using (var frame = tracker.PopResult())
+		{
+			Debug.LogFormat("{0} bodies found.", frame.NumBodies);
+			if (frame.NumBodies > 0)
+			{
+				var bodyId = frame.GetBodyId(0);
+				//Debug.LogFormat("bodyId={0}", bodyId);
+				this.skeleton = frame.GetSkeleton(0);
+				skeletons.Add(this.skeleton);
+				for (var i = 0; i < (int)JointId.Count; i++)
+				{
+					var joint = this.skeleton.Joints[i];
+					var pos = joint.Position;
+					Debug.Log("pos: " + (JointId)i + " " + pos[0] + " " + pos[1] + " " + pos[2]);
+					var rot = joint.Orientation;
+					Debug.Log("rot " + (JointId)i + " " + rot[0] + " " + rot[1] + " " + rot[2] + " " + rot[3]); // Length 4
+					var v = new Vector3(pos[0], -pos[1], pos[2]) * 0.004f;
+					var r = new Quaternion(rot[1], rot[2], rot[3], rot[0]);
+					var obj = debugObjects[i];
+					obj.transform.SetPositionAndRotation(v, r);
+				}
+			}
+		}
+	}
+
+	void averageOutSkeletons()
+	{
+		Debug.Log("creating blockman 2 stuff");
+		blockman2 = GameManager.Instance.blockmanCaptured;
+		foreach (GameObject go in blockman2)
+		{
+			go.SetActive(true);
+		}
+
+		// skeletons is a List<Skeleton> of size 5
+		for (int i = 0; i < (int)JointId.Count; i++)
+		{
+			List<Vector3> positionsOfSameJointPositions = new List<Vector3>();
+
+			for (int j = 0; j < skeletons.Count; j++)
+			{
+				float[] pos = skeletons[j].Joints[i].Position;
+
+				Vector3 posV3 = new Vector3(pos[0], -pos[1], pos[2]) * 0.004f;
+				positionsOfSameJointPositions.Add(posV3);
+
+			}
+			Vector3 averageOfSingleJointI = Vector3Helper.FindAveragePosition(positionsOfSameJointPositions);
+
+			float[] rot = skeletons[0].Joints[i].Orientation;
+			Quaternion rotationOfFirstSkeleton = new Quaternion(rot[1], rot[2], rot[3], rot[0]);
+
+			var jointCube = blockman2[i];
+			jointCube.transform.SetPositionAndRotation(averageOfSingleJointI, rotationOfFirstSkeleton);
+
+		}
+
+		Debug.Log("we have enough skeletons");
+		//GameManager.Instance.currentState = GameState.CaptureCompleted;
+		//Disable this Update loop's logic from running
+		canUpdate = false;
+		//GameManager.Instance.LoadScene((int)SceneEnums.Scenes.GetReady);
+	}
+
 	public void TESTAAJSHDFLJASDHFLKAJSHDFLKAJSDHFKJH()
 	{
+		//enable canUpdate for skeleton capturing
+		canUpdate = true;
 		// load new additive scene/panel
 
 		// load skeleton into new blockman
